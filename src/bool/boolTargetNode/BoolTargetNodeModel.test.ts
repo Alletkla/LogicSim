22f76cefd37d1abe93d40da0687be3626f8636fc
Alletkla <maximilianfoerster@t-online.de>
2023-08-22 16:55:24 +0200

    added tests

--- src/bool/boolTargetNode/BoolTargetNodeModel.test.ts
+++ src/bool/boolTargetNode/BoolTargetNodeModel.test.ts
@@ -1,16 +1,16 @@
 import { beforeEach, describe, expect, test } from "@jest/globals";
-import { BoolSourceNodeModel } from "./BoolSourceNodeModel";
 import { NodeSerializeCycle, purgeIds } from "../../helpers/testEnv";
+import { BoolTargetNodeModel } from "./BoolTargetNodeModel";
 
 
-let node: BoolSourceNodeModel
+let node: BoolTargetNodeModel
 beforeEach(() => {
-    node = new BoolSourceNodeModel("defaultTest", "rgb(255,255,255)")
+    node = new BoolTargetNodeModel("defaultTest", "rgb(255,255,255)")
 })
 
 describe('deserialize', () => {
     test('has same count of ports', () => {
-        const { deserializedNode } = NodeSerializeCycle(BoolSourceNodeModel, node)
+        const { deserializedNode } = NodeSerializeCycle(BoolTargetNodeModel, node)
         expect(Object.values(deserializedNode.getPorts()).length).toBe(2)
         expect(deserializedNode.getInPorts().length).toBe(1)
         expect(deserializedNode.getOutPorts().length).toBe(1)
@@ -19,7 +19,7 @@ describe('deserialize', () => {
 
 describe('cloning', () => {
     test('same besides id', () => {
-        const clone = node.clone() as BoolSourceNodeModel
+        const clone = node.clone() as BoolTargetNodeModel
 
         expect(purgeIds(clone.serialize())).toStrictEqual(purgeIds(node.serialize()))
     })
